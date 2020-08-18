451f8331cde7d4c4a7d4bb61fdc3e397df8a1657
Claudia Jardón <44615865+Crazy4Cats@users.noreply.github.com>
2020-08-18 18:17:13 -0500

    Update bulkCreateEmailless.py

--- bulkCreateEmailless.py
+++ bulkCreateEmailless.py
@@ -1,7 +1,9 @@
 #Create email-less users from a CSV file
 #This script requires the access token to be placed in a file named accessToken
-#The script also requires a list Workplace user names in a file named userNames.csv
+#The script also requires a list Workplace user names in a file named user.csv
 #The format of users.csv is to have 1 user name per line with no empty lines
+#The structure for the first row from CSV file with the users is: email,first name,last name,job title,department,phone number,location,locale,manager email,timezone,external id,organization,division,cost center,start date,frontline
+
 import requests, json, ast
 
 #Get the access token from a file named accessToken
@@ -30,37 +32,42 @@ with open("users.csv", "r") as f:
 scimUser = "https://www.workplace.com/scim/v1/Users/"
 
 #for emailless users, we must leave userName blank and the unique employee ID is mapped to externalId
+#all emailless users are predefined as Frontline Employees, but if the 'frontline' field is empty the user will be excluded from frontline.
 state = {}
 for user in users:
-    if user['"external id"'] != "" or (user['"first name"'] == "" and user['"last name"'] == ""):
-        state['externalId'] = user['"external id"']
+    if user['external id'] != "" or (user['first name'] == "" and user['last name'] == ""):
+        state['externalId'] = user['external id']
         state['active'] = True
-        state['schemas'] = ['urn:scim:schemas:core:1.0','urn:scim:schemas:extension:facebook:auth_method:1.0']
+        state['schemas'] = ['urn:scim:schemas:core:1.0','urn:scim:schemas:extension:facebook:auth_method:1.0','urn:scim:schemas:extension:facebook:frontline:1.0']
         state['urn:scim:schemas:extension:facebook:auth_method:1.0'] = {}
         state['urn:scim:schemas:extension:facebook:auth_method:1.0']['auth_method'] = 'password'
+        state['urn:scim:schemas:extension:facebook:frontline:1.0'] = {}
+        state['urn:scim:schemas:extension:facebook:frontline:1.0']['is_frontline'] = True
+        if user['frontline'] == "":
+            state['urn:scim:schemas:extension:facebook:frontline:1.0']['is_frontline'] = False
         state['name'] = {}
-        if user['"first name"'] != "":
-            state['name']['givenName'] = user['"first name"']
-        if user['"last name"'] != "":
-            state['name']['familyName'] = user['"last name"']
-        state['name']['formatted'] = user['"first name"'] + " " + user['"last name"']
-        if user['"job title"'] != "":
-            state['title'] = user['"job title"']
-        if user['"phone number"'] != "":
+        if user['first name'] != "":
+            state['name']['givenName'] = user['first name']
+        if user['last name'] != "":
+            state['name']['familyName'] = user['last name']
+        state['name']['formatted'] = user['first name'] + " " + user['last name']
+        if user['job title'] != "":
+            state['title'] = user['job title']
+        if user['phone number'] != "":
             state['phoneNumbers'] = {}
             state['phoneNumbers']['type'] = 'work'
             state['phoneNumbers']['primary'] = True
-            state['phoneNumbers']['value'] = user['"phone number"']
+            state['phoneNumbers']['value'] = user['phone number']
         if user['location'] != "":
-            state['addresses'] = {}
-            state['addresses']['primary'] = True
-            state['addresses']['type'] = 'work'
-            state['addresses']['locality'] = user['location']
+            state['address'] = {}
+            state['address']['primary'] = True
+            state['address']['type'] = 'work'
+            state['address']['locality'] = user['location']
         if user['locale'] != "":
             state['locale'] = user['locale']
         if user['timezone'] != "":
             state['timezone'] = user['timezone']
-        if user['department'] != "" or user['organization'] != "" or user['division'] != "" or user['"cost center"'] != "":
+        if user['department'] != "" or user['organization'] != "" or user['division'] != "" or user['cost center'] != "":
             state['schemas'].append('urn:scim:schemas:extension:enterprise:1.0')
             state['urn:scim:schemas:extension:enterprise:1.0'] = {}
             if user['department'] != "":
@@ -69,11 +76,12 @@ for user in users:
                 state['urn:scim:schemas:extension:enterprise:1.0']['organization'] = user['organization']
             if user['division'] != "":
                 state['urn:scim:schemas:extension:enterprise:1.0']['division'] = user['division']
-            if user['"cost center"'] != "":
-                state['urn:scim:schemas:extension:enterprise:1.0']['costCenter'] = user['"cost center"']
-        if user['"start date"'] != "":
+            if user['cost center'] != "":
+                state['urn:scim:schemas:extension:enterprise:1.0']['costCenter'] = user['cost center']
+        if user['start date'] != "":
             state['schemas'].append('urn:scim:schemas:extension:facebook:starttermdates:1.0')
             state['urn:scim:schemas:extension:facebook:starttermdates:1.0'] = {}
-            state['urn:scim:schemas:extension:facebook:starttermdates:1.0']['startDate'] = user['"start date"']
+            state['urn:scim:schemas:extension:facebook:starttermdates:1.0']['startDate'] = user['start date']
         r = requests.post(scimUser, data=json.dumps(state), headers=headers)
         print(r.text)
+
