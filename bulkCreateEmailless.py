afb5cb7eade8bdc9aacb769ca4bb6c80ca9c165c
Claudia Jardón <44615865+Crazy4Cats@users.noreply.github.com>
2020-08-20 12:04:44 -0500

    Update bulkCreateEmailless.py

--- bulkCreateEmailless.py
+++ bulkCreateEmailless.py
@@ -7,26 +7,26 @@
 #If the 'frontline' field is left empty the user won't be assigned to the Frontline People Set. If it has any other character, the user will be added as a Frontline employee.
 
 #Spanish comments:
-#Este script permite crear usuarios sin correo electrónico a partir de un archivo CSV
-#El script necesita que el código de acceso/access token se coloque en un archivo llamado accesstoken
+#Este script permite crear usuarios sin correo electronico a partir de un archivo CSV
+#El script necesita que el codigo de acceso/access token se coloque en un archivo llamado accesstoken
 #Los usuarios se tienen que agregar en un archivo llamado user.csv
-#El archivo no puede tener renglones vacíos y debe de haber sólo un usuario por renglón.
+#El archivo no puede tener renglones vacios y debe de haber solo un usuario por renglon.
 #La estrucura del archivo es la siguiente: email,first name,last name,job title,department,phone number,location,locale,manager email,timezone,external id,organization,division,cost center,start date,frontline
-#Si el campo 'frontline' se deja vacío, el usuario no será asignado al Frontline/Primera Línea. Cualquier otro caracter en ese campo, hará que el usuario sea Frontline/Primera Línea
+#Si el campo 'frontline' se deja vacio, el usuario no sera asignado al Frontline/Primera Linea. Cualquier otro caracter en ese campo, hara que el usuario sea Frontline/Primera Linea
 
 #More info / más información: https://developers.facebook.com/docs/workplace/reference/account-management-api
 
 import requests, json, ast
 
 #Get the access token from a file named accessToken
-#Se obtiene el código de acceso/access token del archivo llamado accessToken.
+#Se obtiene el codigo de acceso/access token del archivo llamado accessToken.
 f = open("accessToken", "r")
 TOKEN = f.read()
 TOKEN = TOKEN.rstrip("\r\n")
 f.close()
 
 #Generate the Authentication header
-#Se genera el Encabezado de Autenticación
+#Se genera el Encabezado de Autenticacion
 AUTH = "Bearer %s" % TOKEN
 headers = {'Authorization': AUTH, 'Content-Type': 'application/json'}
 
@@ -48,7 +48,7 @@ with open("users.csv", "r") as f:
 scimUser = "https://www.workplace.com/scim/v1/Users/"
 
 #for emailless users, we must leave userName blank and the unique employee ID is mapped to externalId
-#Para todos los usuarios sin correo electrónico es necesario dejar el nombre de usuario vacío. El identificador del usuario es mapeado al externalID
+#Para todos los usuarios sin correo electronico es necesario dejar el nombre de usuario vacio. El identificador del usuario es mapeado al externalID
 
 state = {}
 for user in users:
